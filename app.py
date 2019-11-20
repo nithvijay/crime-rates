56350c19c1059880590ea7cd4804d080bdde3aab
Nithin Vijayakumar <nv2ba@virginia.edu>
2019-11-19 20:14:34 -0500

    b

--- app.py
+++ app.py
@@ -1,116 +1,22 @@
 import dash
-import pandas as pd
-import time
 import dash_core_components as dcc
 import dash_html_components as html
-#import plotly_express as px
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
-
-
-
-nyc = pd.read_csv("/Users/nvijayakumar/OneDrive/Workspace/Projects/NodeFinalProject/NYCArrests.csv")
-nyc["ARREST_DATETIME"] = pd.to_datetime(nyc["ARREST_DATE"], cache=True)
-
-sp500 = pd.read_csv("/Users/nvijayakumar/OneDrive/Workspace/Projects/NodeFinalProject/S&P500.csv")
-sp500["Date"] = pd.to_datetime(sp500["Date"])
-
-to_merge1 = sp500.loc[
-    sp500["Date"].isin(nyc["ARREST_DATETIME"].value_counts().index), ["Date", "Close"]
-]
-to_merge2 = (
-    nyc["ARREST_DATETIME"]
-    .value_counts()
-    .reset_index(name="Num Arrests")
-    .rename({"index": "Date"}, axis=1)
-)
-
-daily = pd.merge(to_merge1, to_merge2)
-daily.columns
-
-daily["Year"] = daily["Date"].dt.year
-daily["Week"] = daily["Date"].dt.week
-daily["Month"] = daily["Date"].dt.month
-
-weekly = daily.groupby(["Year", "Week"]).mean().reset_index()
+import pandas as pd
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
+daily = pd.read_csv('https://raw.githubusercontent.com/nithvijay/crime-rates/master/daily.csv')
+weekly = pd.read_csv('https://raw.githubusercontent.com/nithvijay/crime-rates/master/weekly.csv')
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-
-def generate_table(dataframe, max_rows=10):
-    return html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])]
-        +
-        # Body
-        [
-            html.Tr([html.Td(dataframe.iloc[i][col]) for col in dataframe.columns])
-            for i in range(min(len(dataframe), max_rows))
-        ]
-    )
-
-
-app.layout = html.Div(
-    children=[
-        html.H1(children="Dash Test"),
-        # generate_table(sp500),
-        html.Div(
-            children="""
-        Dash: A web application framework for Python.
-    """
-        ),
-        dcc.Markdown(
-            """
-            # Markdown Test
-            This is pretty cool
-            - item 1
-            - item 2
-            *emphasis*
-            """
-        ),
-        html.Div(
-            id="radio-items",
-            children=dcc.RadioItems(
-                id="radio-test",
-                options=[
-                    {"label": "New York City", "value": "NYC"},
-                    {"label": "Montréal", "value": "MTL"},
-                    {"label": "San Francisco", "value": "SF"},
-                ],
-                value="MTL",
-            ),
-        ),
-        #dcc.Graph(id="example-graph"),
-        html.H3(id="graph-title", children="This is the graph title"),
-        dcc.Graph(id="weekly-by-year"),
-        dcc.Slider(
-            id="years-slider",
-            min=weekly["Year"].min(),
-            max=weekly["Year"].max(),
-            value=weekly["Year"].min(),
-            marks={str(year): {"label": str(year)} for year in weekly["Year"].unique()},
-        ),
-        html.Br(),
-    ]
-)
-
-
-# @app.callback(Output("example-graph", "figure"), [Input("radio-test", "value")])
-# def update(title):
-#     return px.scatter(x=sp500.loc[:1000, "Date"], y=sp500.loc[:1000, "Close"])
-
-
-@app.callback(Output("weekly-by-year", "figure"), [Input("years-slider", "value")])
-def update_weekly_app(year):
-    return update_weekly(year)
 
 def update_weekly(year):
     weekly_by_year = weekly.loc[
-        weekly["Year"] == year, ["Week", "Close", "Num Arrests", "Month"]
+        weekly["Year"] == year,
+        ["Week", "Close", "Num Arrests", "Month", "Mean.TemperatureF"],
     ]
     fig = go.Figure()
     fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -155,6 +61,43 @@ def update_weekly(year):
     print(year)
     return fig
 
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Dash Test"),
+        html.Div(
+            children="""
+                     Dash: A web application framework for Python.
+                     """
+        ),
+        html.Div(
+            id="radio-items",
+            children=dcc.RadioItems(
+                id="radio-test",
+                options=[
+                    {"label": "New York City", "value": "NYC"},
+                    {"label": "Montréal", "value": "MTL"},
+                ],
+                value="MTL",
+            ),
+        ),
+        # dcc.Graph(id="example-graph"),
+        html.H3(id="graph-title", children="This is the graph title"),
+        dcc.Graph(id="weekly-by-year"),
+        dcc.Slider(
+            id="years-slider",
+            min=weekly["Year"].min(),
+            max=weekly["Year"].max(),
+            value=weekly["Year"].min(),
+            marks={str(year): {"label": str(year)} for year in weekly["Year"].unique()},
+        ),
+        html.Br(),
+    ]
+)
+
+@app.callback(Output("weekly-by-year", "figure"), [Input("years-slider", "value")])
+def update_weekly_app(year):
+    return update_weekly(year)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
